7098a4d759b31bee60b10b71e933b79f4950cb70
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-01-10 20:33:37 +0900

    Update 5021-왕위 계승.cpp

--- Hash/[백준] 5021-왕위 계승/최아현/5021-왕위 계승.cpp
+++ Hash/[백준] 5021-왕위 계승/최아현/5021-왕위 계승.cpp
@@ -17,35 +17,35 @@ int main() {
 	unordered_map<string, double> hash;
 	vector<string> v;
 
-	cin >> n >> m;
-	cin >> s;
+	cin >> n >> m;  
+	cin >> s;  
 
-	hash[s] = 1.0;
+	hash[s] = 1.0;  //왕 1 대입
 
 	for (int i = 0; i < n ; i++) {
 		
-		cin >> s >> p1 >> p2;
-		if (hash[p1] != NULL && hash[p2] != NULL) {
-			hash[s] = hash[p1] + hash[p2];
+		cin >> s >> p1 >> p2;  // 자녀 부모 받아서
+		if (hash[p1] != NULL && hash[p2] != NULL) {  //값이 존재하면 혈통
+			hash[s] = hash[p1] + hash[p2];  // 혈통계산
 		}
-		else if (hash[p1] != NULL) {
+		else if (hash[p1] != NULL) {  //P1만 혈통
 			hash[s] = hash[p1] / 2;
 		}
-		else if (hash[p2] != NULL) {
+		else if (hash[p2] != NULL) { //P2만 혈통일 경우
 			hash[s] = hash[p2] / 2;
 		}
-		else {
+		else { // 둘다 아닐경우
 			hash[s] = 0.0;
 		}
 	}
 
-	for (int j = 0; j < m; j++) {
+	for (int j = 0; j < m; j++) {  // 후보 받아서 벡터에 후보이름 넣음
 		cin >> s;
 		v.insert(v.begin() + j, s);
 	}
 	king = v[0];
 
-	for (int k = 1; k < m; k++) {
+	for (int k = 1; k < m; k++) {  // 후보이름(키)으로 hash값 찾아서 가장 큰 값 찾기
 		if (hash[king] < hash[v[k]])
 			king = v[k];
 	}
