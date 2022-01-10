a237b2cd45e923d223e7188737d136a6e0c5672b
Wonchul-Shin <72932922+Wonchul-Shin@users.noreply.github.com>
2022-01-10 20:33:57 +0900

    Update 5021 - 왕위계승.cpp

--- Hash/[백준] 5021-왕위 계승/신원철/5021 - 왕위계승.cpp
+++ Hash/[백준] 5021-왕위 계승/신원철/5021 - 왕위계승.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 #include <unordered_map>
 #include<list>
@@ -12,26 +14,26 @@ double find_root(unordered_map<string, array<string,2>> hash, string name, strin
     if (hash.find(name) == hash.end()){ // 귀족이 아닐 때 == 족보를 찾지 못할 때
         return 0; 
     }
-    return find_root(hash, hash[name][0],  maker) / 2 + find_root(hash, hash[name][1],  maker) / 2;
+    return find_root(hash, hash[name][0],  maker) / 2 + find_root(hash, hash[name][1],  maker) / 2; // 두명의 수치를 더한 후 2를 나누면 된다.
 }
 
 int main()
 {
-    unordered_map<string, array<string,2>> hash;
+    unordered_map<string, array<string,2>> hash; // hash에 이름과 부모이름 
 
     int n, m;
     string word1, word2, word3;
     cin >> n >> m;
     string arr [2];
-    double answer_num = 0;
+    double answer_num = 0; // 새로운 왕의 수치 담을 변수
     double temp = 0;
-    string answer;
-    string maker;
+    string answer; // 새로운 왕의 이름
+    string maker; // 왕
 
     cin >> maker;
     for (int i = 0; i < n; i++){
         cin >> word1 >> word2 >> word3;
-        hash[word1] = {word2, word3};
+        hash[word1] = {word2, word3}; 
     }
     for (int i = 0; i < m; i++){
         cin >> word1;
