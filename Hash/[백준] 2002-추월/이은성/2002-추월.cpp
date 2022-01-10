99c2496ddd4a518a35b7e1f807a59bfeb0f3b343
Ganbilly <94830725+Ganbilly@users.noreply.github.com>
2022-01-10 20:27:06 +0900

    Update 2002-추월.cpp

--- Hash/[백준] 2002-추월/이은성/2002-추월.cpp
+++ Hash/[백준] 2002-추월/이은성/2002-추월.cpp
@@ -10,20 +10,21 @@ int main(){
     unordered_map<string, int> m;
 
     cin >> n;
-    arr = new int[n];
-    for(int i = 0; i < n; i++){
+    arr = new int[n];                                      // n개 입력받고 int형배열동적 할당
+    
+    for(int i = 0; i < n; i++){                            // 터널 들어간 순서 입력 
         cin >> s;
         m[s] = i;
     }
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){                             // 나온 순서 입력
         cin >> s;
         arr[i] = m[s];
     }
 
     index = 0;
 
-    for(int i = 0 ; i < n; i++){
+    for(int i = 0 ; i < n; i++){                            // 앞서간 차량 찾기
         for(int j = index; j < n; j++){
             if(arr[j] == i){
                 count += j - index ;
