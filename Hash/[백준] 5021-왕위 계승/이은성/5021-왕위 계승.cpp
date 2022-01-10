a29dd4567a1b2fc544551f12d2646c0318406d76
Ganbilly <94830725+Ganbilly@users.noreply.github.com>
2022-01-10 20:31:08 +0900

    Update 5021-왕위 계승.cpp

--- Hash/[백준] 5021-왕위 계승/이은성/5021-왕위 계승.cpp
+++ Hash/[백준] 5021-왕위 계승/이은성/5021-왕위 계승.cpp
@@ -13,10 +13,10 @@ int main(){
     int n, m, tier;
     string name;
     string s1, s2, s3;
-    map<string, pair <string, string>> map;
-    vector< pair <double, string>> checkName;
+    map<string, pair <string, string>> map;                             // map[아들] = { 부모, 부모 } 의 형태로 저장
+    vector< pair <double, string>> checkName;                           //checkName =>  <피의농도 , 사람이름>의 형태로 저장  
 
-    cin >> n >> m;
+    cin >> n >> m;                                          
     cin >> name;
 
     for(int i = 0; i < n; i++){
@@ -40,16 +40,16 @@ int main(){
 double findtier(map<string, pair <string, string>> map, int tier, string name, string checkName){
     double a, b;
 
-    if(map.find(checkName) == map.end())
+    if(map.find(checkName) == map.end())                                // 부모 정보가 map에 없으면 0 리턴
         return 0;
     
-    else if(map[checkName].first == name)
+    else if(map[checkName].first == name)                               // elseif에서 부모중 한쪽에 전대 왕이 있어도 반대편 부모의 농도계산후 덧셈 
         return (1.0/pow(2,tier + 1)) + findtier(map, tier + 1, name, map[checkName].second);
         
     else if(map[checkName].second == name)
         return (1.0/pow(2,tier + 1)) + findtier(map, tier + 1, name, map[checkName].first);
     
-    a = findtier(map, tier + 1, name, map[checkName].first);
+    a = findtier(map, tier + 1, name, map[checkName].first);            // 양쪽 부모 중에 어느쪽도 전대 왕이 아니면 각각의 부모의 농도를 구해서 더함
     b = findtier(map, tier + 1, name, map[checkName].second);   
     
 
