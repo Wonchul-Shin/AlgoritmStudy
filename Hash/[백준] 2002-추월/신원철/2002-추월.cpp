a1612aa136829f9e5e8228cf8b59a90ffd4af3bd
Wonchul-Shin <72932922+Wonchul-Shin@users.noreply.github.com>
2022-01-10 20:29:28 +0900

    Update 2002-추월.cpp

--- Hash/[백준] 2002-추월/신원철/2002-추월.cpp
+++ Hash/[백준] 2002-추월/신원철/2002-추월.cpp
@@ -20,25 +20,25 @@ int main()
 
     string word;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){ 
         cin >> word;
-        hash[word] = i;
-        chain.push_back(i);
+        hash[word] = i; // 해시의 키로 인덱스를 설정함 0 1 2 3 4 5
+        chain.push_back(i); // 리스트에도 배열을 담음 0 1 2 3 4 5 
     }
     iter = chain.begin();
-    for(int i = 0; i < n; i++){
-        cin >> word;
-        iter = chain.begin();
+    for(int i = 0; i < n; i++){ // for 문을 돌면서 
+        cin >> word; // word를 받고
+        iter = chain.begin(); // 포인터는 리스트의 가장 앞의 값으로 
 
-        if (hash[word] == *iter){
-            chain.pop_front();
+        if (hash[word] == *iter){ // 만약 해시의 키값이 인덱스와 같다면 (순서를 지켰다면)
+            chain.pop_front(); // 리스트에서 pop
 
         }
-        else{
+        else{ // 해시의 키값이 인덱스와 같지 않다면
             for(iter=chain.begin(); iter != chain.end(); iter++){
-                if(hash[word] == *iter){
-                    chain.erase(iter);
-                    answer++;
+                if(hash[word] == *iter){ // 리스트에서 해당 값을 찾아서 
+                    chain.erase(iter); // 지우고
+                    answer++; // 답을 하나 올린다. 
                     break;
                 }
             }
