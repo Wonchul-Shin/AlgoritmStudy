97e2d1fd8c985a8ef8071dc207e89e831e4a6d17
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-01-10 20:29:45 +0900

    Update 2002-추월.cpp

--- Hash/[백준] 2002-추월/최아현/2002-추월.cpp
+++ Hash/[백준] 2002-추월/최아현/2002-추월.cpp
@@ -17,26 +17,26 @@ int main() {
 	cin >> n;
 
 	unordered_map<string, int> hash;
-	out_car.insert(out_car.begin(), 0);
+	out_car.insert(out_car.begin(), 0); //헷갈릴 것 같아서 0대입
 
 	for (i = 1; i <= n; i++) {
-		cin >> s;
+		cin >> s;               //들어간 순서대로 차량번호와 1, 2, 3, 4 로 표시
 		hash[s] = i;
 	}
 
 	for (j = 1; j <= n; j++) {
-		cin >> s;
+		cin >> s;                 //들어간 순서를 가지고 있는 차량이름 찾아서 나온대로 백터에 넣음
 		out_car.insert(out_car.begin() + j, hash[s]);
 	}
 
 	int temp;
 	i = 1; 
 
-	while (i <= n) {
+	while (i <= n) { //정렬 
 		for (int k = 1; k < n - 1; k++) {
-			if (out_car[k] > out_car[k + 1]) {
+			if (out_car[k] > out_car[k + 1]) {  //일단 큰수가 앞에있으면 카운터 세고
 				count++;
-				for (j = 1; j < n; j++) {
+				for (j = 1; j < n; j++) {  //한번 정렬
 					if (out_car[j] > out_car[j + 1]) {
 						temp = out_car[j];
 						out_car[j] = out_car[j + 1];
