78fcce042c7965ffa2698a4b9160516cfbae05ef
Blackstar <85343553+YongJ7@users.noreply.github.com>
2022-01-10 20:27:27 +0900

    Update 추월.cpp

--- Hash/[백준] 2002-추월/용제/추월.cpp
+++ Hash/[백준] 2002-추월/용제/추월.cpp
@@ -13,15 +13,17 @@ int main()
 	int N, cnt = 0;
 	string s;
 	cin >> N;
+	// 해쉬 테이블에 차량 번호와 들어가는 순서 입력
 	for (int i = 0; i < N; i++) {
 		cin >> s;
 		arr.insert({ s,i });
 	}
+	// 차량이 나오는 순서를 배열에 저장
 	for (int i = 0; i < N; i++) {
 		cin >> s;
 		v.push_back(s);
 	}
-
+	// 나온 순서대로 확인하여 나온 순서가 들어간 순서보다 빠른 차량 확인
 	for (int i = 0; i < N; i++) {
 		for (int j = i + 1; j < N; j++) {
 			if (arr[v[i]] > arr[v[j]]) {
