d7769b6896668f5da5a7b6b5072e9d185914c762
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-01-20 18:43:45 +0900

    Update 3015번-오아시스 재결합.cpp

--- Stack/[백준] 3015번 오아시스 재결합/최아현/3015번-오아시스 재결합.cpp
+++ Stack/[백준] 3015번 오아시스 재결합/최아현/3015번-오아시스 재결합.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	int N;
 	int in;
 	vector<int> v;
@@ -16,26 +15,20 @@ int main() {
 	int count = 0;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		cin >> in;
-		v.insert(v.begin() + i, in);
+	for (int i = 0; i < N; i++) { // 맨 앞사람부터 뒷사람을 한명씩 확인하면서 
+		cin >> in;                // 그 사이에 큰사람이 없으면 +1해서 총 값을 result에 저장
+		v.insert(v.begin() + i, in); 
 	}
 
 	while (point < N) {
 
-		for (int i = point + 2; i < N; i++) {
-			count = 0;
+		for (int i = point + 2; i < N; i++) {  // 바로 앞사람은 보이니깐 마지막에 한꺼번에 + 해주고 
+			count = 0;                         // 그 다음사람 부터 확인
 			for (int j = point + 1; j < i; j++) {
-				if (v[point] < v[j] && v[i] < v[j]) {
+				if (v[point] < v[j] || v[i] < v[j]) {  //두 사람 사이에 A 또는 B보다 키가 큰 사람이 없어야 한다
 					break;
 				}
-				else if (v[point] == v[j] && v[i] < v[j]) {
-					break;
-				}
-				else if (v[point] < v[j] && v[i] == v[j]) {
-					break;
-				}
-				if (j == i - 1){
+				if (j == i - 1){ //for문이 다 돌아갈 때까지 break안 나가면 +1 
 					count++;
 				}
 			}
