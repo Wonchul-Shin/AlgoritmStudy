8a0689d0dd208cbac9c110ab41c4f969f15aa3b1
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-01-20 18:21:19 +0900

    Add files via upload

---
+++ Stack/[백준] 3015번 오아시스 재결합/최아현/3015번 오아시스 재결합.cpp
@@ -0,0 +1,45 @@
+/*#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int n;
+long long result;
+stack<pair<long long, long long>> s; //키, 중복된 개수
+
+int main() {
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		long long h;
+		cin >> h;
+
+		int count = 1;
+		while (!s.empty()) {
+			if (s.top().first < h) {
+				result += s.top().second;
+				count = 1;
+				s.pop();
+			}
+			else if (s.top().first == h) {
+				result += s.top().second;
+				count = s.top().second + 1;
+				s.pop();
+			}
+			else {
+				result++;
+				break;
+			}
+		}
+		s.push(make_pair(h, count));
+	}
+
+	cout << result;
+
+	return 0;
+}*/
