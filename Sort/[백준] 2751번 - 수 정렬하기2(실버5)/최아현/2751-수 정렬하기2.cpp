5c47112ea50e5a6ceb9b9790c041f201650c7e20
ahyc01 <96827937+ahyc01@users.noreply.github.com>
2022-02-01 02:30:02 +0900

    Update 2751-수 정렬하기2.cpp

--- Sort/[백준] 2751번 - 수 정렬하기2(실버5)/최아현/2751-수 정렬하기2.cpp
+++ Sort/[백준] 2751번 - 수 정렬하기2(실버5)/최아현/2751-수 정렬하기2.cpp
@@ -25,3 +25,35 @@ int main() {
 	}
 	return 0;
 }
+
+
+----------------
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> v;
+
+	int N, x;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		v.insert(v.begin() + i, x); // 값 삽입
+	}
+	sort(v.begin(), v.end()); // 정렬
+
+	for (int i = 0; i < N; i++) {
+		cout << v[i] << '\n';
+	}
+	return 0;
+}
