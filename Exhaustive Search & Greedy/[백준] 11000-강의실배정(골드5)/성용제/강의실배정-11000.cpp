3c9ba2d2eeb43a6f9ff42d8f3e2493f9b465eb37
Blackstar <85343553+YongJ7@users.noreply.github.com>
2022-02-10 17:50:41 +0900

    Update 강의실배정-11000.cpp

--- Exhaustive Search & Greedy/[백준] 11000-강의실배정(골드5)/성용제/강의실배정-11000.cpp
+++ Exhaustive Search & Greedy/[백준] 11000-강의실배정(골드5)/성용제/강의실배정-11000.cpp
@@ -7,9 +7,9 @@ int main()
 
     //freopen("input.txt","r",stdin);
     int N, end, begin;
-
-    vector<pair<int, int>> schedule;
-    priority_queue<int, vector<int>, greater<int> > pq;
+    
+    vector<pair<int, int>> schedule;  // 모든 일정 시작과 끝 시간 저장
+    priority_queue<int, vector<int>, greater<int> > pq;  // 끝나는 시간 오름차순 저장
 
     cin >> N;
 
@@ -21,19 +21,20 @@ int main()
 
     sort(schedule.begin(), schedule.end());
 
-    pq.push(schedule[0].second);
-
+    pq.push(schedule[0].second);  // 가장 먼저 시작 하는 강의의 끝나는 시간을 저장
+    
+    // 강의 시작 시간에 대해서 오름 차순으로 저장 된 배열을 탐색하여 다음 강의가 시작할 때 가장 먼저 끝나는 강의실을 사용할 수 있는지 없는지 판별
     for (int i = 1; i < N; i++)
     {
-        if (pq.top() <= schedule[i].first)
+        if (pq.top() <= schedule[i].first)  // 다음 강의 시작 시간이 현재 강의가 끝난 이후라면 현 강의를 지우고 다음 강의 끝 시간 저장
         {
             pq.pop();
             pq.push(schedule[i].second);
         }
-        else{
+        else{  // 강의 중간에 시작해야 한다면 새로운 강의실을 열고 끝 시간 저장
             pq.push(schedule[i].second);
         }
     }
 
-    cout << pq.size();
+    cout << pq.size(); // 남은 강의가 결국 필요한 강의실 수
 }
